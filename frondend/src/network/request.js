8ef1fb1b08a6734d181ef4e67606b88646eadb57
LawssssCat <1191693505@qq.com>
2020-03-11 17:34:40 +0800

    添加：拦截器测试

--- frondend/src/network/request.js
+++ frondend/src/network/request.js
@@ -8,6 +8,23 @@ export function request (config) {
     timeout: 5000
   })
 
+  // axios 的拦截器
+  instance.interceptors.request.use(
+    config => { // config 是 请求的配置信息
+      console.log('request interceptor success', config)
+      return config
+    },
+    err => {
+      console.log('request interceptor err', err)
+    }
+  )
+  instance.interceptors.response.use(response => {
+    console.log('response interceptor success', response)
+    return response
+  }, err => {
+    console.log('response interceptor err', err)
+  })
+
   // 发送真正的网络请求
   return instance(config)
 }
